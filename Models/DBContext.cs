4d927502fe974ad1c290488130d33885e9d40898
chesswondo <chesswondo@gmail.com>
2024-09-28 19:54:05 +0300

    Добавьте файлы проекта.

---
+++ Models/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAppDBMS.Models
+{
+    public partial class DBContext : DbContext
+
+    {
+
+        public DBContext(DbContextOptions<DBContext> options)
+                    : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,52 @@
+﻿/*namespace WebAppDBMS
+{
+    public class Startup
+    {
+    }
+}*/
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using WebAppDBMS.Models;
+
+namespace DBMSWebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DBContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllersWithViews();
+        }
+
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Databases}/{action=Index}/{id?}");
+
+            });
+        }
+    }
+}
